fcd31aa988d2efc0086f96903ceeaf13dc97469f
sf <sf@dev.com>
2025-06-16 22:31:59 +0530

    remove group chat list

--- app/home/UserList/_layout.tsx
+++ app/home/UserList/_layout.tsx
@@ -33,6 +33,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
+       <Tabs.Screen
+        name="Settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gear" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
