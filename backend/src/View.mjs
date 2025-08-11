6882e59b42377014d785af92244602ca5fbb037b
sf <sf@dev.com>
2025-08-12 00:12:15 +0400

    udate view

--- backend/src/View.mjs
+++ backend/src/View.mjs
@@ -22,24 +22,18 @@ export default class View {
         await host.addWriter(b4a.from(value.add, 'hex'), { indexer: true })
         if (this.VERBOSE_LOGGING) console.log('Writer and indexer added successfully:', value.add)
       }
-
-      if (value.gets) {
-        const all = []
-        for (const delta of value.gets) {
-          const seq = view.length - delta
-          if (seq < 0 || view.length <= seq) continue
-          // console.log('waiting for a block', seq, view.length, view.signedLength)
-          const val = JSON.parse(await view.get(seq))
-          // console.log('got it!')
-          all.push({ seq, value })
-        }
-        const id = `${Date.now()}}`
-        await view.put(id, { gets: all })
+      if (value.remove) {
+        if (this.VERBOSE_LOGGING) console.log('Processing remove writer command for:', value.remove)
+        // Remove as both writer AND indexer
+        await host.removeWriter(b4a.from(value.remove, 'hex'), { indexer: true })
+        if (this.VERBOSE_LOGGING) console.log('Writer and indexer removed successfully:', value.remove)
       }
-
-      // Store all messages in the view
-      const id = `${Date.now()}`
-      await view.put(id, { echo: value })
+      if (value.add) {
+        await view.put(value.id, value)
+      }
+      if (value.delete) {
+        await view.delete(value.id)
+      }
     }
   }
 
